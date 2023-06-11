d3e3d44bcba879cdf96ef7b76c9757bee1c3b17f
Godavari Bandgar <bandgar 101@gmail.com>
2023-06-11 19:30:37 +0530

    six commit

---
+++ src/main/java/com/bikkadit/electronicstore/customvalidation/ImageNameValid.java
@@ -0,0 +1,28 @@
+package com.bikkadit.electronicstore.customvalidation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD,ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = ImageNameValidator.class)
+public @interface ImageNameValid {
+    //error message
+     String message() default "Invalid Email name!!";
+
+
+     //represent group of constraints
+    Class<?>[] groups() default { };
+
+    //additional information about annotations
+    Class<? extends Payload>[] payload() default { };
+
+
+
+
+
+
+
+}
