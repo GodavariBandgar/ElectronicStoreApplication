dd0366c7076ee39f0b3deef3daae59142b18018d
GodavariBandgar <bandgar101@gmail.com>
2023-06-23 18:19:44 +0530

    validations on product dto

--- src/main/java/com/bikkadit/electronicstore/dtos/ProductDto.java
+++ src/main/java/com/bikkadit/electronicstore/dtos/ProductDto.java
@@ -1,7 +1,9 @@
 package com.bikkadit.electronicstore.dtos;
 
+import com.bikkadit.electronicstore.customvalidation.ImageNameValid;
 import com.bikkadit.electronicstore.entities.Category;
 import lombok.*;
+import org.springframework.lang.NonNull;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -16,15 +18,21 @@ public class ProductDto {
     @Size(min = 3,max = 27,message = "Invalid name")
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "Description must be required")
     private String description;
 
+    @NotBlank(message = "price will be required")
     private double price;
+    @NotBlank(message = "must be required")
     private int discountedPrice;
+
     private int quantity;
+
     private Date addedDate;
+    @NotBlank
     private boolean live;
     private boolean stock;
+    @ImageNameValid
     private String productImageName;
     private CategoryDto category;
 }
