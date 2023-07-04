cb845cab4a4db8c96275ee2d3b4bdb89598f94ae
GodavariBandgar <bandgar101@gmail.com>
2023-07-04 21:56:06 +0530

    unit testing of service layer update user

---
+++ src/test/java/com/bikkadit/electronicstore/service/serviceImpl/UserServiceImplTest.java
@@ -0,0 +1,140 @@
+package com.bikkadit.electronicstore.service.serviceImpl;
+
+import com.bikkadit.electronicstore.dtos.PageableResponse;
+import com.bikkadit.electronicstore.dtos.UserDto;
+import com.bikkadit.electronicstore.entities.User;
+import com.bikkadit.electronicstore.repository.UserRepository;
+import com.bikkadit.electronicstore.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UserServiceImplTest {
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private UserService userService;
+
+    User user;
+
+    @BeforeEach
+    public void init(){
+        user=User.builder().name("Godavari")
+                .email("bandgar@gmail.com")
+                .about("This is testing create user")
+                .gender("Female")
+                .imagename("abc.png")
+                .password("goda")
+                .build();
+    }
+
+    @Test
+    void createUser() {
+        //arrange
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+
+        //Act
+        UserDto user1 = userService.createUser(modelMapper.map(user, UserDto.class));
+        System.out.println(user1.getName());
+        Assertions.assertNotNull(user1);
+
+        //Assert
+        Assertions.assertEquals("Godavari",user1.getName());
+
+    }
+
+
+
+    @Test
+    void updateUser() {
+        String userId ="abcd";
+        UserDto userDto=UserDto.builder()
+                .name("Godavari Bandgar")
+                .about("This is updated user details")
+                .gender("Female")
+                .imagename("xyz.png")
+                .build();
+
+        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+        UserDto updatedUser = userService.updateUser(userDto, userId);
+        System.out.println(updatedUser.getName());
+        Assertions.assertNotNull(userDto);
+
+
+
+    }
+
+    @Test
+    void deleteUser() {
+
+        String userId="userIdabc";
+        Mockito.when(userRepository.findById("userIdabc")).thenReturn(Optional.of(user));
+        userService.deleteUser(userId);
+        Mockito.verify(userRepository,Mockito.times(1)).delete(user);
+
+    }
+
+    @Test
+    void getAllUser() {
+
+      User  user1=User.builder().name("Dilip")
+                .email("bandgar@gmail.com")
+                .about("This is testing create user")
+                .gender("Female")
+                .imagename("abc.png")
+                .password("goda")
+                .build();
+      User  user2=User.builder().name("Pari")
+                .email("bandgar@gmail.com")
+                .about("This is testing create user")
+                .gender("Female")
+                .imagename("abc.png")
+                .password("goda")
+                .build();
+
+        List<User> userList= Arrays.asList(user,user1,user2);
+        Page<User> page=new PageImpl<>(userList);
+
+
+        Mockito.when(userRepository.findAll((Pageable) Mockito.any())).thenReturn(page);
+
+        PageableResponse<UserDto> allUser = userService.getAllUser(1, 2, "name", "asc");
+        Assertions.assertEquals(3,allUser.getContent().size());
+
+
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getUserByEmail() {
+    }
+
+    @Test
+    void searchUser() {
+
+
+    }
+}
