33604624c19d581925abc59cbfcd3baaa6bf7184
GodavariBandgar <bandgar101@gmail.com>
2023-07-04 21:56:42 +0530

    unit testing of service layer delete user

---
+++ src/test/java/com/bikkadit/electronicstore/service/UserServiceTest.java
@@ -0,0 +1,20 @@
+package com.bikkadit.electronicstore.service;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UserServiceTest {
+
+    //create user
+    @Test
+    public void createUserTest() {
+
+
+    }
+
+}
