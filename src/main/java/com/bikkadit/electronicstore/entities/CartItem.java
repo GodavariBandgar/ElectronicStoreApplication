518131390b7614bb33152abdc1b3f0caa46ba2a9
GodavariBandgar <bandgar101@gmail.com>
2023-07-18 11:55:16 +0530

    Cart Entity and CartItem Entity model completed

---
+++ src/main/java/com/bikkadit/electronicstore/entities/CartItem.java
@@ -0,0 +1,28 @@
+package com.bikkadit.electronicstore.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cartItemId;
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    private int totalPrice;
+    //mapping cart
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+}
