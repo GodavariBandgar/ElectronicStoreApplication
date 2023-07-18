137a9d3f28daf81e756c1a4823456dd30b99b122
GodavariBandgar <bandgar101@gmail.com>
2023-07-18 19:15:28 +0530

    cart Controller api completed

---
+++ src/main/java/com/bikkadit/electronicstore/controller/CartController.java
@@ -0,0 +1,54 @@
+package com.bikkadit.electronicstore.controller;
+
+import com.bikkadit.electronicstore.dtos.AddItemToCartRequest;
+import com.bikkadit.electronicstore.dtos.CartDto;
+import com.bikkadit.electronicstore.payloads.ApiResponse;
+import com.bikkadit.electronicstore.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    //add item to cart
+    @PostMapping("/userId")
+    public ResponseEntity<CartDto> addItemToCart(@PathVariable String userId, @RequestBody AddItemToCartRequest request){
+        CartDto cartDto = cartService.addItemToCart(userId, request);
+        return new ResponseEntity<>(cartDto, HttpStatus.OK);
+    }
+    @DeleteMapping("/{userId}/items/{itemId}")
+    public ResponseEntity<ApiResponse> removeItemFromCart( @PathVariable String userId,@PathVariable int itemId){
+      cartService.removeItemFromCart(userId, itemId);
+
+        ApiResponse response = ApiResponse.builder()
+                .message("item is required!!")
+                .success(true)
+                .status(HttpStatus.OK)
+                .build();
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    //clear cart
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> clearCart( @PathVariable String userId) {
+        cartService.clearCart(userId);
+
+        ApiResponse response = ApiResponse.builder()
+                .message("cart is not blank!!")
+                .success(true)
+                .status(HttpStatus.OK)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    //add item to cart
+    @GetMapping("/userId")
+    public ResponseEntity<CartDto> getCart(@PathVariable String userId) {
+        CartDto cartDto = cartService.getCartByUser(userId);
+        return new ResponseEntity<>(cartDto, HttpStatus.OK);
+    }
+}
